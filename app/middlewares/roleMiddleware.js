5a453a20b4a8b9d1189f896c7f7c29891f9a98e3
KRATICOS <UTTI232030@UTVCO.EDU.MX>
2025-11-05 18:15:01 -0600

    Add role verification middleware

---
+++ app/middlewares/roleMiddleware.js
@@ -0,0 +1,12 @@
+function verificarRol(rolesPermitidos) {
+  return (req, res, next) => {
+    if (!rolesPermitidos.includes(req.user.rol)) {
+      return res.status(403).json({
+        message: 'Acceso denegado: no tienes permisos suficientes.'
+      });
+    }
+    next();
+  };
+}
+
+module.exports = verificarRol;
