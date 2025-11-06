056af86426df9e77bb389c652e1ad17ec557e234
KRATICOS <UTTI232030@UTVCO.EDU.MX>
2025-11-05 18:51:30 -0600

    Update inventarioRoutes.js

--- app/routes/inventarioRoutes.js
+++ app/routes/inventarioRoutes.js
@@ -11,22 +11,22 @@ const { verificarRol } = require('../middlewares/roleMiddleware');
 // ==========================
 
 // Obtener todos los equipos (usuarios autenticados)
-router.get('/', authMiddleware, inventarioController.obtenerEquipos);
+router.get('/', inventarioController.obtenerEquipos);
 
 // Obtener un equipo por ID (usuarios autenticados)
-router.get('/:id', authMiddleware, inventarioController.obtenerEquipoPorId);
+router.get('/:id', inventarioController.obtenerEquipoPorId);
 
 // Obtener por número de serie (usuarios autenticados)
-router.get('/por-serie/:nseries', authMiddleware, inventarioController.obtenerPorNumeroSerie);
+router.get('/por-serie/:nseries', inventarioController.obtenerPorNumeroSerie);
 
 // Buscar por categoría (usuarios autenticados)
-router.get('/categoria/:categoria', authMiddleware, inventarioController.obtenerPorCategoria);
+router.get('/categoria/:categoria',  inventarioController.obtenerPorCategoria);
 
 // Buscar por estado (usuarios autenticados)
-router.get('/estado/:estado', authMiddleware, inventarioController.obtenerPorEstado);
+router.get('/estado/:estado',  inventarioController.obtenerPorEstado);
 
 // Actualizar estado por código QR (usuarios autenticados)
-router.put('/qr/:codigoQR', authMiddleware, inventarioController.actualizarEstadoPorQR);
+router.put('/qr/:codigoQR', inventarioController.actualizarEstadoPorQR);
 
 
 // ==========================
@@ -36,7 +36,6 @@ router.put('/qr/:codigoQR', authMiddleware, inventarioController.actualizarEstad
 // Crear equipo (solo administradores)
 router.post(
   '/crear',
-  authMiddleware,
   verificarRol(['admin']),
   upload.any(),
   inventarioController.registrarEquipoConImagenes
